d71fdd302980754bbb69bf28217aa4f119cbd0b9
Vladimir Musikhin <hey@vipho.io>
2022-05-30 16:47:20 +0400

    dev06

---
+++ develop/dev06/errors.go
@@ -0,0 +1,5 @@
+package main
+
+import "errors"
+
+var ErrIncorrectFile = errors.New("incorrect file")
--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +22,58 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type Flags struct {
+	F int
+	D string
+	S bool
+}
 
+func main() {
+	flagF := flag.Int("f", 0, "")
+	flagD := flag.String("d", " ", "")
+	flagS := flag.Bool("s", false, "")
+
+	flag.Parse()
+
+	f := Flags{
+		F: *flagF,
+		D: *flagD,
+		S: *flagS,
+	}
+
+	filename := ""
+	args := flag.Args()
+	if len(args) > 1 {
+		log.Fatalln(ErrIncorrectFile)
+	}
+	if len(args) == 1 {
+		filename = args[0]
+	}
+
+	input := ""
+	if filename != "" {
+		bytes, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input = string(bytes)
+	} else {
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input = string(bytes)
+	}
+
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		cols := strings.Split(line, f.D)
+
+		if f.F > 0 && f.F <= len(cols) {
+			fmt.Println(cols[f.F-1])
+		} else if !f.S {
+			fmt.Println(line)
+		}
+	}
 }
