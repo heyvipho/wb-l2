fdce95e7ca419154736b0481cd520fda48fc8968
Vladimir Musikhin <hey@vipho.io>
2022-06-03 20:59:52 +0400

    builder

--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -1,7 +1,60 @@
-package pattern
+package main
+
+import "fmt"
 
 /*
 	Реализовать паттерн «строитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
 */
+
+func main() {
+	product := new(Product)
+
+	director := Director{&ConcreteBuilder{product}}
+	director.Construct()
+
+	result := product.Show()
+
+	fmt.Println(result)
+}
+
+type Builder interface {
+	MakeHeader(str string)
+	MakeBody(str string)
+	MakeFooter(str string)
+}
+
+type Director struct {
+	builder Builder
+}
+
+func (d *Director) Construct() {
+	d.builder.MakeHeader("Header")
+	d.builder.MakeBody("Body")
+	d.builder.MakeFooter("Footer")
+}
+
+type ConcreteBuilder struct {
+	product *Product
+}
+
+func (b *ConcreteBuilder) MakeHeader(str string) {
+	b.product.Content += "<header>" + str + "</header>"
+}
+
+func (b *ConcreteBuilder) MakeBody(str string) {
+	b.product.Content += "<article>" + str + "</article>"
+}
+
+func (b *ConcreteBuilder) MakeFooter(str string) {
+	b.product.Content += "<footer>" + str + "</footer>"
+}
+
+type Product struct {
+	Content string
+}
+
+func (p *Product) Show() string {
+	return p.Content
+}
