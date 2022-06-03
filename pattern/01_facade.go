cd470e5135b7434df569a0318a7a953fa143175f
Vladimir Musikhin <hey@vipho.io>
2022-06-03 20:19:56 +0400

    facade

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -1,7 +1,64 @@
-package pattern
+package main
+
+import (
+	"fmt"
+	"strings"
+)
 
 /*
 	Реализовать паттерн «фасад».
 Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
 */
+
+func main() {
+	man := NewMan()
+
+	result := man.Todo()
+
+	fmt.Println(result)
+}
+
+func NewMan() *Man {
+	return &Man{
+		house: &House{},
+		tree:  &Tree{},
+		child: &Child{},
+	}
+}
+
+type Man struct {
+	house *House
+	tree  *Tree
+	child *Child
+}
+
+func (m *Man) Todo() string {
+	result := []string{
+		m.house.Build(),
+		m.tree.Grow(),
+		m.child.Born(),
+	}
+	return strings.Join(result, "\n")
+}
+
+type House struct {
+}
+
+func (h *House) Build() string {
+	return "Build house"
+}
+
+type Tree struct {
+}
+
+func (t *Tree) Grow() string {
+	return "Tree grow"
+}
+
+type Child struct {
+}
+
+func (c *Child) Born() string {
+	return "Child born"
+}
