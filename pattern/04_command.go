c2994fe52a4754847cf74247ad37e43e2b9b286c
Vladimir Musikhin <hey@vipho.io>
2022-06-03 21:19:35 +0400

    command

--- pattern/04_command.go
+++ pattern/04_command.go
@@ -1,7 +1,74 @@
-package pattern
+package main
+
+import "fmt"
 
 /*
-	Реализовать паттерн «комманда».
+	Реализовать паттерн «команда».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Command_pattern
 */
+
+func main() {
+	invoker := &Invoker{}
+	receiver := &Receiver{}
+
+	invoker.StoreCommand(&ToggleOnCommand{receiver: receiver})
+	invoker.StoreCommand(&ToggleOffCommand{receiver: receiver})
+
+	result := invoker.Execute()
+
+	fmt.Println(result)
+}
+
+type Command interface {
+	Execute() string
+}
+
+type ToggleOnCommand struct {
+	receiver *Receiver
+}
+
+func (c *ToggleOnCommand) Execute() string {
+	return c.receiver.ToggleOn()
+}
+
+type ToggleOffCommand struct {
+	receiver *Receiver
+}
+
+func (c *ToggleOffCommand) Execute() string {
+	return c.receiver.ToggleOff()
+}
+
+type Receiver struct {
+}
+
+func (r *Receiver) ToggleOn() string {
+	return "Toggle On"
+}
+
+func (r *Receiver) ToggleOff() string {
+	return "Toggle Off"
+}
+
+type Invoker struct {
+	commands []Command
+}
+
+func (i *Invoker) StoreCommand(command Command) {
+	i.commands = append(i.commands, command)
+}
+
+func (i *Invoker) UnStoreCommand() {
+	if len(i.commands) != 0 {
+		i.commands = i.commands[:len(i.commands)-1]
+	}
+}
+
+func (i *Invoker) Execute() string {
+	var result string
+	for _, command := range i.commands {
+		result += command.Execute() + "\n"
+	}
+	return result
+}
