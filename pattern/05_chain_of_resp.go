9c3bb50f828a1462265816a5fba8c9e6bd90db69
Vladimir Musikhin <hey@vipho.io>
2022-06-05 14:27:53 +0400

    chain of resp

--- pattern/05_chain_of_resp.go
+++ pattern/05_chain_of_resp.go
@@ -1,7 +1,64 @@
-package pattern
+package main
+
+import "fmt"
 
 /*
 	Реализовать паттерн «цепочка вызовов».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
 */
+
+func main() {
+	handlers := &ConcreteHandlerA{
+		next: &ConcreteHandlerB{
+			next: &ConcreteHandlerC{},
+		},
+	}
+
+	result := handlers.SendRequest(2)
+
+	fmt.Println(result)
+}
+
+type Handler interface {
+	SendRequest(message int) string
+}
+
+type ConcreteHandlerA struct {
+	next Handler
+}
+
+func (h *ConcreteHandlerA) SendRequest(message int) (result string) {
+	if message == 1 {
+		result = "Im handler 1"
+	} else if h.next != nil {
+		result = h.next.SendRequest(message)
+	}
+	return
+}
+
+type ConcreteHandlerB struct {
+	next Handler
+}
+
+func (h *ConcreteHandlerB) SendRequest(message int) (result string) {
+	if message == 2 {
+		result = "Im handler 2"
+	} else if h.next != nil {
+		result = h.next.SendRequest(message)
+	}
+	return
+}
+
+type ConcreteHandlerC struct {
+	next Handler
+}
+
+func (h *ConcreteHandlerC) SendRequest(message int) (result string) {
+	if message == 3 {
+		result = "Im handler 3"
+	} else if h.next != nil {
+		result = h.next.SendRequest(message)
+	}
+	return
+}
