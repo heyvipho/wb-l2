39568456b8a1876e6690d43f8540f72eee6c2efa
Vladimir Musikhin <hey@vipho.io>
2022-06-05 15:16:45 +0400

    state

--- pattern/08_state.go
+++ pattern/08_state.go
@@ -1,7 +1,55 @@
-package pattern
+package main
+
+import "fmt"
 
 /*
 	Реализовать паттерн «состояние».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/State_pattern
 */
+
+func main() {
+	mobile := NewMobileAlert()
+
+	fmt.Println(mobile.Alert())
+	fmt.Println(mobile.Alert())
+
+	mobile.SetState(&MobileAlertSong{})
+
+	fmt.Println(mobile.Alert())
+}
+
+type MobileAlertStater interface {
+	Alert() string
+}
+
+type MobileAlert struct {
+	state MobileAlertStater
+}
+
+func (a *MobileAlert) Alert() string {
+	return a.state.Alert()
+}
+
+func (a *MobileAlert) SetState(state MobileAlertStater) {
+	a.state = state
+}
+
+func NewMobileAlert() *MobileAlert {
+	return &MobileAlert{state: &MobileAlertVibration{}}
+}
+
+type MobileAlertVibration struct {
+}
+
+func (a *MobileAlertVibration) Alert() string {
+	return "Brrr... Brrr... Brrr..."
+}
+
+// MobileAlertSong implements beep alert
+type MobileAlertSong struct {
+}
+
+func (a *MobileAlertSong) Alert() string {
+	return "До... Ре... Ми..."
+}
