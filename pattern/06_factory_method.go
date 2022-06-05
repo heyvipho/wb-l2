1ad4f9221fcddf4f3bdf590ac8b501670ad476f1
Vladimir Musikhin <hey@vipho.io>
2022-06-05 14:39:26 +0400

    factory method

--- pattern/06_factory_method.go
+++ pattern/06_factory_method.go
@@ -1,7 +1,87 @@
-package pattern
+package main
+
+import (
+	"fmt"
+	"log"
+)
 
 /*
 	Реализовать паттерн «фабричный метод».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Factory_method_pattern
 */
+
+func main() {
+	factory := NewCreator()
+
+	products := []string{
+		factory.CreateProduct(A).Use(),
+		factory.CreateProduct(B).Use(),
+		factory.CreateProduct(C).Use(),
+	}
+
+	fmt.Println(products)
+}
+
+type action string
+
+const (
+	A action = "A"
+	B action = "B"
+	C action = "C"
+)
+
+type Creator interface {
+	CreateProduct(action action) Product
+}
+
+type Product interface {
+	Use() string
+}
+
+type ConcreteCreator struct{}
+
+func NewCreator() Creator {
+	return &ConcreteCreator{}
+}
+
+func (p *ConcreteCreator) CreateProduct(action action) Product {
+	var product Product
+
+	switch action {
+	case A:
+		product = &ConcreteProductA{string(action)}
+	case B:
+		product = &ConcreteProductB{string(action)}
+	case C:
+		product = &ConcreteProductC{string(action)}
+	default:
+		log.Fatalln("Unknown Action")
+	}
+
+	return product
+}
+
+type ConcreteProductA struct {
+	action string
+}
+
+func (p *ConcreteProductA) Use() string {
+	return p.action
+}
+
+type ConcreteProductB struct {
+	action string
+}
+
+func (p *ConcreteProductB) Use() string {
+	return p.action
+}
+
+type ConcreteProductC struct {
+	action string
+}
+
+func (p *ConcreteProductC) Use() string {
+	return p.action
+}
